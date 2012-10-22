a6fff0741b2d19d675ab3352eba2a58139ce31ee
samwize <junda@just2us.com>
2012-10-22 16:21:08 +0800

    Convert tabs to spaces

--- auto-attendant.php
+++ auto-attendant.php
@@ -20,111 +20,111 @@ $hoiio = new HoiioService($HOIIO_APP_ID, $HOIIO_ACCESS_TOKEN);
 // Look at $app_state and decide what to do now
 $app_state = $_POST["app_state"];
 switch ($app_state) {
-	case NULL:
-		// State: A call comes in
-		// Action: Answer the call, play a welcome message, and gather key response
-		$notify = $hoiio->parseIVRNotify($_POST);
-		$session = $notify->getSession();
-		$text = $TEXT_WELCOME_MESSAGE . formDirectoryText($directory);
-		// Gather for a single digit. Repeat 3 times max.
-		$hoiio->ivrGather($session, $THIS_SERVER_URL . '?app_state=gather', $text, 1, 10, 3);
-		break;
+    case NULL:
+        // State: A call comes in
+        // Action: Answer the call, play a welcome message, and gather key response
+        $notify = $hoiio->parseIVRNotify($_POST);
+        $session = $notify->getSession();
+        $text = $TEXT_WELCOME_MESSAGE . formDirectoryText($directory);
+        // Gather for a single digit. Repeat 3 times max.
+        $hoiio->ivrGather($session, $THIS_SERVER_URL . '?app_state=gather', $text, 1, 10, 3);
+        break;
 
-	case 'gather':
-		// State: User has pressed a key
-		// Action: Transfer accordingly
-		$notify = $hoiio->parseIVRNotify($_POST);
-		$session = $notify->getSession();
-		$key = $notify->getDigits();
-		$transfer_to = $directory[$key];
-		switch ($transfer_to[0]) {
-			case NULL:
-				// Invalid key. Retry gather
-				$text = $TEXT_INVALID_KEY . ' ' . formDirectoryText($directory);
-				$hoiio->ivrGather($session, $THIS_SERVER_URL . '?app_state=gather', $text, 1, 10, 3);
-				break;
-			case 'SMSALERT':
-				// Send an SMS Alert and hangup
-				$hoiio->sms($MY_MOBILE_NUMBER, 'You have received a call from ' . $_POST['from'], $SMS_SENDER_NAME);
-				$hoiio->ivrHangup($session, $THIS_SERVER_URL . '?app_state=hangup', $TEXT_SMS_ALERT_SENT);
-				break;
-			case 'VOICEMAIL':
-				// Record a voicemail
-				$hoiio->ivrRecord($session, $THIS_SERVER_URL . '?app_state=record', $TEXT_RECORD_VOICEMAIL);
-				break;
-			default:
-				// Transfer
-				$hoiio->ivrTransfer($session, $transfer_to[0], $THIS_SERVER_URL . '?app_state=transfer', $TEXT_TRANSFERRING, '', '', 'continue');
-				break;
-		}
-		break;
+    case 'gather':
+        // State: User has pressed a key
+        // Action: Transfer accordingly
+        $notify = $hoiio->parseIVRNotify($_POST);
+        $session = $notify->getSession();
+        $key = $notify->getDigits();
+        $transfer_to = $directory[$key];
+        switch ($transfer_to[0]) {
+            case NULL:
+                // Invalid key. Retry gather
+                $text = $TEXT_INVALID_KEY . ' ' . formDirectoryText($directory);
+                $hoiio->ivrGather($session, $THIS_SERVER_URL . '?app_state=gather', $text, 1, 10, 3);
+                break;
+            case 'SMSALERT':
+                // Send an SMS Alert and hangup
+                $hoiio->sms($MY_MOBILE_NUMBER, 'You have received a call from ' . $_POST['from'], $SMS_SENDER_NAME);
+                $hoiio->ivrHangup($session, $THIS_SERVER_URL . '?app_state=hangup', $TEXT_SMS_ALERT_SENT);
+                break;
+            case 'VOICEMAIL':
+                // Record a voicemail
+                $hoiio->ivrRecord($session, $THIS_SERVER_URL . '?app_state=record', $TEXT_RECORD_VOICEMAIL);
+                break;
+            default:
+                // Transfer
+                $hoiio->ivrTransfer($session, $transfer_to[0], $THIS_SERVER_URL . '?app_state=transfer', $TEXT_TRANSFERRING, '', '', 'continue');
+                break;
+        }
+        break;
 
-	case 'transfer':
-		// State: Transferring
-		// Action: If failed, play a message
-		if ($_POST['transfer_status'] != 'answered') {
-			$notify = $hoiio->parseIVRNotify($_POST);
-			$session = $notify->getSession();
-			// If could not transfer, we ask to retry
-			$hoiio->ivrGather($session, $THIS_SERVER_URL . '?app_state=gather', $TEXT_TRANSFER_FAILED, 1, 10, 3);		
-		}
-		break;
+    case 'transfer':
+        // State: Transferring
+        // Action: If failed, play a message
+        if ($_POST['transfer_status'] != 'answered') {
+            $notify = $hoiio->parseIVRNotify($_POST);
+            $session = $notify->getSession();
+            // If could not transfer, we ask to retry
+            $hoiio->ivrGather($session, $THIS_SERVER_URL . '?app_state=gather', $TEXT_TRANSFER_FAILED, 1, 10, 3);       
+        }
+        break;
 
-	case 'record':
-		// State: Recorded voicemail
-		// Action: Send an SMS
-		$recordUrl = $_POST['record_url';
-		$newVoiceMail = "New Voicemail from " . $_POST['from'] . ": " . $recordUrl];
-		appendToVoiceMail($newVoiceMail);
-		// $post_body = file_get_contents('php://input');
-		// appendToVoiceMail(http_build_query($_POST));
-		if ($_POST['record_url'] != NULL) {
-			$shortenUrl = shortenUrl($recordUrl, $GOOGLE_URL_SHORTENER_API_KEY);
-			$hoiio->sms($MY_MOBILE_NUMBER, 'You have received a voicemail from ' . $_POST['from'] . '. Listen at ' . $shortenUrl, $SMS_SENDER_NAME);
-		}
-		break;
+    case 'record':
+        // State: Recorded voicemail
+        // Action: Send an SMS
+        $recordUrl = $_POST['record_url';
+        $newVoiceMail = "New Voicemail from " . $_POST['from'] . ": " . $recordUrl];
+        appendToVoiceMail($newVoiceMail);
+        // $post_body = file_get_contents('php://input');
+        // appendToVoiceMail(http_build_query($_POST));
+        if ($_POST['record_url'] != NULL) {
+            $shortenUrl = shortenUrl($recordUrl, $GOOGLE_URL_SHORTENER_API_KEY);
+            $hoiio->sms($MY_MOBILE_NUMBER, 'You have received a voicemail from ' . $_POST['from'] . '. Listen at ' . $shortenUrl, $SMS_SENDER_NAME);
+        }
+        break;
 }
 
 /** Return the text "Press 1 to ... Press 2 to ..." **/
 function formDirectoryText($directory) {
-	$text = '';
-	foreach ($directory as $key => $value) {
-		$text = $text . ' Press ' . $key . ' ' . $value[1]. '.';
-	}
-	return $text;
+    $text = '';
+    foreach ($directory as $key => $value) {
+        $text = $text . ' Press ' . $key . ' ' . $value[1]. '.';
+    }
+    return $text;
 }
 
 /** Append a line of text in voicemail.txt **/
 function appendToVoiceMail($text) {
-	$myFile = "voicemail.txt";
-	$fh = fopen($myFile, 'a') or die("can't open file");
-	fwrite($fh, $text . "\n");
-	fclose($fh);	
+    $myFile = "voicemail.txt";
+    $fh = fopen($myFile, 'a') or die("can't open file");
+    fwrite($fh, $text . "\n");
+    fclose($fh);    
 }
 
 /** Shorten URL **/
 function shortenUrl($longUrl, $apiKey = '') {
-	$postData = array('longUrl' => $longUrl, 'key' => $apiKey);
-	$jsonData = json_encode($postData);
-	 
-	$curlObj = curl_init();
-	 
-	curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url');
-	curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($curlObj, CURLOPT_HEADER, 0);
-	curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
-	curl_setopt($curlObj, CURLOPT_POST, 1);
-	curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
-	 
-	$response = curl_exec($curlObj);
-	 
-	//change the response json string to object
-	$json = json_decode($response);
-	 
-	curl_close($curlObj);
-	 
-	return $json->id;
+    $postData = array('longUrl' => $longUrl, 'key' => $apiKey);
+    $jsonData = json_encode($postData);
+     
+    $curlObj = curl_init();
+     
+    curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url');
+    curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curlObj, CURLOPT_HEADER, 0);
+    curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+    curl_setopt($curlObj, CURLOPT_POST, 1);
+    curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+     
+    $response = curl_exec($curlObj);
+     
+    //change the response json string to object
+    $json = json_decode($response);
+     
+    curl_close($curlObj);
+     
+    return $json->id;
 }
 
 ?>
--- config-sample.php
+++ config-sample.php
@@ -1,30 +1,30 @@
 <?php
 
 // Important information. Change as needed.
-$HOIIO_APP_ID 		= "";		// Get from http://developer.hoiio.com
-$HOIIO_ACCESS_TOKEN = "";		// Get from http://developer.hoiio.com
-$HOIIO_NUMBER 		= "";		// Hoiio Number that you bought eg. +16501234567
-$MY_MOBILE_NUMBER 	= "";		// Your personal mobile number eg. +16501234567
-$THIS_SERVER_URL 	= "";		// Your server URL eg. http://www.example.com/myapp/auto-attendant.php
+$HOIIO_APP_ID       = "";       // Get from http://developer.hoiio.com
+$HOIIO_ACCESS_TOKEN = "";       // Get from http://developer.hoiio.com
+$HOIIO_NUMBER       = "";       // Hoiio Number that you bought eg. +16501234567
+$MY_MOBILE_NUMBER   = "";       // Your personal mobile number eg. +16501234567
+$THIS_SERVER_URL    = "";       // Your server URL eg. http://www.example.com/myapp/auto-attendant.php
 
 // Text being read. Change as you like, or use the default.
-$MY_NAME 				= "Junda";
-$TEXT_WELCOME_MESSAGE 	= "Hello. Welcome to " . $MY_NAME . "'s auto attendant.";
-$TEXT_TRANSFERRING 		= 'Please wait while I transfer your call.';
-$TEXT_TRANSFER_FAILED 	= 'Sorry, the call was not answered. Please try again.';
-$TEXT_INVALID_KEY 		= 'You have entered an invalid option. Please try again.';
-$TEXT_RECORD_VOICEMAIL 	= 'Please leave your voice message after the beep.';
-$TEXT_SMS_ALERT_SENT	= $MY_NAME . ' has received your SMS. He will call you back shortly. Goodbye!';
+$MY_NAME                = "Junda";
+$TEXT_WELCOME_MESSAGE   = "Hello. Welcome to " . $MY_NAME . "'s auto attendant.";
+$TEXT_TRANSFERRING      = 'Please wait while I transfer your call.';
+$TEXT_TRANSFER_FAILED   = 'Sorry, the call was not answered. Please try again.';
+$TEXT_INVALID_KEY       = 'You have entered an invalid option. Please try again.';
+$TEXT_RECORD_VOICEMAIL  = 'Please leave your voice message after the beep.';
+$TEXT_SMS_ALERT_SENT    = $MY_NAME . ' has received your SMS. He will call you back shortly. Goodbye!';
 
 // Your transfer directory
 $directory = array(
-	'1'=>array($MY_MOBILE_NUMBER,	"to reach " . $MY_NAME . "'s office phone"),
-	'2'=>array($MY_MOBILE_NUMBER,	"to reach " . $MY_NAME . "'s mobile phone"),
-	'3'=>array('SMSALERT',			"to send " . $MY_NAME . " an SMS, and he will call you back"),
-	'4'=>array('VOICEMAIL',			"to leave " . $MY_NAME . " a voice mail message"),
+    '1'=>array($MY_MOBILE_NUMBER,   "to reach " . $MY_NAME . "'s office phone"),
+    '2'=>array($MY_MOBILE_NUMBER,   "to reach " . $MY_NAME . "'s mobile phone"),
+    '3'=>array('SMSALERT',          "to send " . $MY_NAME . " an SMS, and he will call you back"),
+    '4'=>array('VOICEMAIL',         "to leave " . $MY_NAME . " a voice mail message"),
 );
 
 // Others
-$SMS_SENDER_NAME 	= $MY_MOBILE_NUMBER;
-$GOOGLE_URL_SHORTENER_API_KEY	= '';
+$SMS_SENDER_NAME    = $MY_MOBILE_NUMBER;
+$GOOGLE_URL_SHORTENER_API_KEY   = '';
 ?>
